ed68849dec81dd1d6e07615e642546342bdd4d9e
Nishesh <nsingla3_be23@thapar.edu>
2025-08-27 19:57:47 +0530

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+  unoptimized: true,
+},
 };
 
 export default nextConfig;
